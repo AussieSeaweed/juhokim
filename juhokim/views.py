f785edc72553614171f1a9f86a432f9b15d28330
Juho Kim <juho-kim@outlook.com>
2021-01-03 22:04:24 -0500

    trimmed down requirements.txt

--- juhokim/views.py
+++ juhokim/views.py
@@ -7,4 +7,4 @@ class HomeView(PostListView):
     template_name = 'juhokim/home.html'
 
     def get_queryset(self):
-        return super().get_queryset()[:settings.NUM_RECENT_POSTS]
+        return super().get_queryset()[:settings.RECENT_POST_COUNT]
