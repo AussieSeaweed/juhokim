710ac5a2da5dbc58514fa644940db948fa62949e
Juho <juho-kim@outlook.com>
2021-06-08 21:40:47 -0400

    Reset Project

--- blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from blog.views import PortfolioView, PostDetailView, PostListView
-
-urlpatterns = [
-    path('posts/', PostListView.as_view(), name='post-list'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('portfolio/', PortfolioView.as_view(), name='portfolio'),
-]
