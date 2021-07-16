2a1d058a688fd21c99768ef02aaae0de88203ef2
Juho <juho-kim@outlook.com>
2021-07-15 21:11:05 -0400

    Fixed settings

--- juhokim/settings.py
+++ juhokim/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'urls'
+ROOT_URLCONF = 'juhokim.urls'
 
 TEMPLATES = [
     {
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Toronto'
 
 USE_I18N = True
 
